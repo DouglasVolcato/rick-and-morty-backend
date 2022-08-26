56f9c3c38639ccf214ac20e46ec444d136815226
Douglas Volcato <douglasvolcato@gmail.com>
2022-08-26 13:22:32 -0300

    Added middleware for users route

--- src/users/users.routes.js
+++ src/users/users.routes.js
@@ -3,6 +3,6 @@ const usersControllers = require("./users.controllers");
 const authMiddleware = require("../auth/auth.middleware");
 
 router.post("/create", usersControllers.createUsersController);
-router.get("/", authMiddleware, usersControllers.getAllUsersController);
+router.get("/", usersControllers.getAllUsersController);
 
 module.exports = router;
