2923f121e9f0431b7b82bfeea2edbf8851f4eef0
Douglas Volcato <douglasvolcato@gmail.com>
2022-08-26 13:17:43 -0300

    Fixed response errors

--- src/users/users.routes.js
+++ src/users/users.routes.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const usersControllers = require("./users.controllers");
+const authMiddleware = require("../auth/auth.middleware");
 
 router.post("/create", usersControllers.createUsersController);
-router.get("/", usersControllers.getAllUsersController);
+router.get("/", authMiddleware, usersControllers.getAllUsersController);
 
 module.exports = router;
